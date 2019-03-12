fdbd7fdad509ce3cc23a39467e6f0a0db23347a2
Rodrigo <cherry199662@gmail.com>
2019-03-11 22:30:34 -0500

    Adding pages for portalio and blog

---
+++ src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styles from "./404.module.scss"
+import Layout from "../components/Layout/Layout"
+import {Link} from 'gatsby'
+export default () => (
+  <Layout>
+    <div className={styles.content}>
+      <h1 className={styles.header}>Page not found</h1>
+      <p className={styles.errorMessage}>The page you are looking for does not
+        exists.</p>
+      <Link to='/'>Home</Link>
+    </div>
+  </Layout>
+)
