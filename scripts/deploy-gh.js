56784caa7e54361f1e2919b1596c995cc1bb766d
Jhair Rodrigo Viveros Cordova <cherry199662@gmail.com>
2020-07-21 23:46:27 -0500

    chore: update dependencies

--- scripts/deploy-gh.js
+++ scripts/deploy-gh.js
@@ -1,21 +1,11 @@
-const ghpages = require("gh-pages")
 const fs = require("fs")
 const path = require("path")
-const argv = require('yargs').argv
-const message = argv.m !== true && argv.m || 'Done';
+const argv = require("yargs").argv
+const message = (argv.m !== true && argv.m) || "Done"
 
 // CNAME will be created or overwritten by default.
-fs.copyFileSync(path.resolve("CNAME"),
-  path.resolve(__dirname, "../public", "CNAME"))
-
-ghpages.publish(
-  "public",
-  {
-    branch: "master",
-    repo: "https://github.com/rodrigovive/rodrigovive.github.io",
-    message
-  },
-  () => {
-    console.log("Deploy Complete!")
-  },
+fs.copyFileSync(
+  path.resolve("CNAME"),
+  path.resolve(__dirname, "../public", "CNAME")
 )
+
